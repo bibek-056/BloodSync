df35777af149303b2d2a97d5045c3cb726d14029
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-09-15 15:34:32 +0545

    Fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <>
-    <div className="bg-gray-100 min-h-screen">
-     <Navbar/>
-     <div className="h-[700px]">
+      <div className="bg-gray-100 min-h-screen">
+        <Navbar />
+
+        <Footer />
       </div>
-     <Footer/>
-     </div>
     </>
-  )
+  );
 }
