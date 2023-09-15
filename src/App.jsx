5e3e51020493948c7961d9f499a3f897cbc28561
bibek-056 <adh.bibek056@gmail.com>
2023-09-15 16:12:15 +0545

    Project Intro Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Project from "./components/Project";
 
 export default function App() {
   return (
     <>
     <div className="bg-gray-100 min-h-screen">
      <Navbar/>
+     <Project/>
      </div>
     </>
   )
