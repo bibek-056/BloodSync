09c8a3e0c36c288b4ec53a6a4f816d9426cb1e23
bibek-056 <adh.bibek056@gmail.com>
2023-09-15 18:47:08 +0545

    Project Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Project from './components/Project';
 
 export default function App() {
   return (
     <>
       <div className="bg-gray-100 min-h-screen">
         <Navbar />
-
+        <Project/>
         <Footer />
       </div>
     </>
