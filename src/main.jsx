59041a03357c4c51678a35397b978a076d480a96
bibek-056 <adh.bibek056@gmail.com>
2023-09-19 12:01:46 +0545

    Build Index

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import ghpages from 'gh-pages';
+
+ghpages.publish('dist', function(err) {});
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
